0c0a96b899d2c23e41ab3996eda4f29e90a594ba
mehedi1256 <mehedihassanshovospirit@gmail.com>
2024-03-17 20:27:25 +0600

    parameter route

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel 8 tutorial</title>
+    <link rel="stylesheet" href="/app.css" >
+</head>
+<body>
+<article>
+    <h1><a href="/posts/my-first-post">My first posts</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores distinctio dolorem enim, fuga fugit laudantium obcaecati pariatur, possimus quisquam quod repellendus repudiandae vero vitae? Assumenda aut corporis debitis dolor enim est eum fuga, incidunt ipsum labore maiores, non, odio officia perferendis praesentium provident quia quis quod similique totam ut veniam!
+    </p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-second-post">My Second posts</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores distinctio dolorem enim, fuga fugit laudantium obcaecati pariatur, possimus quisquam quod repellendus repudiandae vero vitae? Assumenda aut corporis debitis dolor enim est eum fuga, incidunt ipsum labore maiores, non, odio officia perferendis praesentium provident quia quis quod similique totam ut veniam!
+    </p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-third-post">My Third posts</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores distinctio dolorem enim, fuga fugit laudantium obcaecati pariatur, possimus quisquam quod repellendus repudiandae vero vitae? Assumenda aut corporis debitis dolor enim est eum fuga, incidunt ipsum labore maiores, non, odio officia perferendis praesentium provident quia quis quod similique totam ut veniam!
+    </p>
+</article>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view("posts");
 });
+
+Route::get('/posts/{post}', function ($slug) {
+    $path = __DIR__ . "./../resources/posts/{$slug}.html";
+
+    if (! file_exists($path)) {
+//        abort(404);
+        return redirect('/');
+    }
+
+    $post = file_get_contents($path);
+    return view("post", [
+        "post" => $post
+    ]);
+})->where('post', '[A-z_\-]+');
